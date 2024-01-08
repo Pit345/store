e7141699e57f549f5787986e8c0dfa7ea8ebb295
Pit345 <p1312402@gmail.com>
2024-01-08 11:11:51 +0200

    view cart

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from store import views
+from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<product_id>/', views.detail, name='detail'),
+    path('my_cart/', views.my_cart, name='my_cart'),
+    path('<product_id>/', views.show, name='show'),
     path('add/<product_id>/', views.add_to_cart, name='add_to_cart')
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from store.models import Product, User, CartItem
 
 # Create your views here.
@@ -7,9 +7,15 @@ def index(request):
     products = Product.objects.all()
     return render(request, 'store/index.html', {'products': products})
 
-def detail(request, product_id):
+def show(request, product_id):
     product = Product.objects.get(id=product_id)
-    return render(request, 'store/detail.html', {'product': product})
+    return render(request, 'store/show.html', {'product': product})
 
 def add_to_cart(request, product_id):
-    breakpoint()
+    product = Product.objects.get(id=product_id)
+    cartitem = CartItem.objects.create(user= request.user, product=product)
+    return HttpResponse(f"Пользователь № {cartitem.user_id} add to cart {cartitem.product_id}")
+
+def my_cart(request):   
+    products_user = CartItem.objects.filter(user_id=request.user.id)
+    return render(request, 'store/my_cart.html', {'products_user': products_user})
