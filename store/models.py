074291b1c712cf28026e9daaba497f4ded9f820c
Pit345 <p1312402@gmail.com>
2024-02-22 02:01:01 +0200

    delete unique=true

--- store/models.py
+++ store/models.py
@@ -19,7 +19,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 class CartItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, unique=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
 
