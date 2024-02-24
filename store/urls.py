60663ff1959b6ba742527e37436852c74e50daaf
Pit345 <p1312402@gmail.com>
2024-02-24 11:32:03 +0200

    add models order, order_item

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_categories, name='all_categories'),
+    path('create_order/', views.create_order, name='create_order'),
     path('products/<slug:category_slug>/', views.products_category, name='products_category'),
-    path('product/<slug:product_slug>/', views.view_product, name='view_product')
+    path('product/<slug:product_slug>/', views.view_product, name='view_product'),
 ]
--- store/views.py
+++ store/views.py
@@ -3,6 +3,9 @@ from store.models import Product, User, CartItem, Category, Cart
 from django.contrib import messages
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+from .forms import OrderForm
+from .models import *
 
 # Create your views here.
 
@@ -18,3 +21,22 @@ def products_category(request, category_slug):
 def view_product(request, product_slug):
     product = Product.objects.get(slug=product_slug)
     return render(request, 'store/view_product.html', {'product': product})
+
+def create_order(request):
+    if request.method == 'GET':
+        order_form = OrderForm()
+        return render(request, 'store/create_order.html', {'order_form': order_form})
+    else:
+        cart = get_object_or_404(Cart, user=request.user)
+        order_form = OrderForm(request.POST)
+        if order_form.is_valid():
+            order = Order.objects.create(name=order_form.cleaned_data.get('name'),
+                                        phone_number=order_form.cleaned_data.get('phone_number'),
+                                        payment_method=order_form.cleaned_data.get('payment_method'),
+                                        user = request.user)
+            
+            for cart_item in cart.cartitem_set.all():
+                OrderItem.objects.create(order=order, product=cart_item.product, 
+                                         quantity = cart_item.quantity, price=cart_item.product.price)
+
+            return redirect(reverse('my_cart'))
